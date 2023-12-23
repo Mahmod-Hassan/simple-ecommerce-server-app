96ba97b02b790aa65711d7c80488086ee55c9fff
Mahmod Hassan <mahmodhasan7788@gmail.com>
2023-12-23 10:17:02 +0600

    create a dynamic route for getting single product by id

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,12 +2,12 @@ const mongoose  = require("mongoose");
 const productSchema = require("../Schemas/productSchema");
 const express = require('express');
 
-const Product = new mongoose.model("Products", productSchema);
+const Products = new mongoose.model("Products", productSchema);
 const router = express.Router();
 
 router.get('/products', async (req, res) => {
    try {
-     const products = await Product.find();
+     const products = await Products.find();
      if(products){
       res.status(200).send(products)
      }else{
@@ -15,10 +15,23 @@ router.get('/products', async (req, res) => {
         message: 'products not found'
       })
      }
-     res.send(products)
    } catch (error) {
        res.status(500).send({message: error.message})
    }
 })
-
+router.get('/products/:id', async (req, res) => {
+  const id = req.params.id;
+   try {
+    const product = await Products.findOne({_id: id});
+    if(product){
+      res.status(200).send(product)
+     }else{
+      res.status(404).send({
+        message: 'product not found'
+      })
+     }
+   } catch (error) {
+    res.status(500).send({message: error.message})
+   }
+})
 module.exports = router;
