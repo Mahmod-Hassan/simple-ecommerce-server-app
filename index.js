15a788c44cb498f367dba672d4919cae633ba212
Mahmod Hassan <mahmodhasan7788@gmail.com>
2023-12-23 22:50:12 +0600

    create login and signup endpoints and stored user to database successfully

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const port = 4000 || 7000;
 
 // import routing file
-const productRouter = require('./routes/product.route')
+const productRouter = require('./routes/product.route');
+const authRouter = require('./routes/auth.route');
 // express app initialization
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // telling express use these router file
 app.use(productRouter);
+app.use(authRouter);
 
 // mongodb uri
 let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcblope.mongodb.net/mobiles?retryWrites=true&w=majority`
@@ -31,11 +33,30 @@ async function connectDB () {
 }
 connectDB();
   
-
+// get root path and send a message
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-// listening server
+
+// if path not match this middleware will catch the error
+app.use((req, res, next) => {
+    // when we will give inside next('somthing')
+    // express will consider it as an error
+    // this error will goes to our custom error handler
+  next('requested url is not found');
+})
+
+// my custom error handler (middleware)
+const errorHandler = ((err, req, res, next) => {
+  if(res.headersSent){
+    return next(err)
+  }
+  res.send({ error: err, message: 'error from my custom errorHndler' });
+})
+app.use(errorHandler);
+
+
+// listening server on port 5000
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
 })
---
+++ routes/auth.route.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bcrypt = require("bcrypt");
+const userSchema = require('../Schemas/userSchema');
+const router = express.Router();
+const User = new mongoose.model("User", userSchema);
+
+
+router.post('/signup', async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+        const oldUser = await User.findOne({email});
+        if(oldUser){
+          return res.send({error: 'user already exist'})
+        }else{
+            const hashPassword = await bcrypt.hash(password, 10);
+            const newUser = new User({
+                name, email, password: hashPassword
+            })
+            const data = await newUser.save();
+            res.send(data)
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({message: 'authentication failed'})
+    }
+})
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email}).select({_id:0});
+        if(user){
+            const validPassword = await bcrypt.compare(password, user.password);
+            if(validPassword){
+              res.send(user);
+            }else{
+              return res.send({message: 'password did not match'})
+            }
+        }else{
+            return res.send({message: 'user not found'})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({message: 'authentication failed line 77'})
+    }
+})
+
+module.exports = router;
